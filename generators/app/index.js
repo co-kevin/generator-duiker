273c00ebd6cfaa276c6435d4d85527e95a56ae06
zhanghua <zhanghua@91zdan.com>
2017-10-30 20:05:57 +0800

    Add templates for app

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,20 +1,66 @@
-var Generator = require('yeoman-generator')
+const Generator = require('yeoman-generator')
+const _string = require('lodash/string')
 
 module.exports = class extends Generator {
   // The name `constructor` is important here
   constructor(args, opts) {
     // Calling the super constructor is important so our generator is correctly set up
     super(args, opts);
-
-    // Next, add your custom code
-    this.option('babel'); // This method adds support for a `--babel` flag
   }
 
-  method1() {
-    this.log('method 1 just ran');
+  prompting() {
+    return this.prompt([{
+      type    : 'input',
+      name    : 'group',
+      message : 'Your project group',
+      default : this.appgroup, // Default to current folder name
+      store   : true
+    }, {
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname, // Default to current folder name
+      store   : true
+    }]).then((answers) => {
+      this._run(answers)
+    })
   }
 
-  method2() {
-    this.log('method 2 just ran');
+  // callback when answered
+  _run (answers) {
+    answers.packagePath = answers.group.replace('\.', '/')
+    answers.nameUpperFirst = _string.upperFirst(answers.name)
+    console.log(answers)
+    // this._copyStaticFiles()
   }
+
+  _copyTplJava(data) {
+    this.fs.copyTpl(this.templatePath('src/main/package/_Application.java')
+      , this.destinationPath(`src/main/${data.packagePath}/${data.name}/${data.nameUpperFirst}Application.java`, data))
+  }
+
+  _copyTplYml() {
+
+  }
+
+  _copyStaticFiles() {
+    let files = [
+      'gradle/wrapper/gradle-wrapper.jar',
+      'gradle/wrapper/gradle-wrapper.properties',
+      '.editorconfig',
+      '.gitignore',
+      'gradlew',
+      'gradlew.bat'
+    ]
+
+    for (let file of files) {
+      this.fs.copy(this.templatePath(file), this.destinationPath(file))
+    }
+  }
+}
+
+function run () {
+  this.fs.copyTpl(this.templatePath('_build.gradle'), this.destinationPath(`build.gradle`))
+  this.fs.copyTpl(this.templatePath('gradle/wrapper/gradle-wrapper.jar'), this.destinationPath(`gradle/wrapper/gradle-wrapper.jar`))
+  this.fs.copyTpl(this.templatePath('gradle/wrapper/gradle-wrapper.properties'), this.destinationPath(`gradle/wrapper/gradle-wrapper.properties`))
 }
