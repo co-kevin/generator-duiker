0ee44ae35174720d28372a8980b133cd7fb2b722
hookszhang <hookszhang@gmail.com>
2017-11-16 13:11:36 +0800

    Update template

--- generators/app/templates/src/main/java/package/_Application.java
+++ generators/app/templates/src/main/java/package/_Application.java
@@ -1,17 +1,14 @@
-package <%= group %>.<%= name %>;
+package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class <%= nameUpperFirst %>Application {
+public class <%= nameCases.hump %>Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(<%= nameUpperFirst %>Application.class, args);
+        SpringApplication.run(<%= nameCases.hump %>Application.class, args);
     }
 }
