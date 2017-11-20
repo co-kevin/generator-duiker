12316e0895274c24c226f96b8de71f7f8b7e89b6
hookszhang <hookszhang@gmail.com>
2017-11-20 11:02:24 +0800

    Add method _copyModel

--- generators/entity/templates/_Model.java
+++ generators/entity/templates/_Model.java
@@ -1,4 +1,4 @@
-package com.zdan91.smart.bill.model;
+package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.model;
 
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
@@ -12,36 +12,24 @@ import java.util.Date;
  * 表描述
  */
 @Data
-@Table(name = "sample_model")
-public class SampleModel {
+@Table(name = "<%= tableName %>")
+public class <%= tableName %> {
+    <%_ for (var i = 0; i < results.length; i++) { _%>
+        <%_ const result = results[i] _%>
+
+        <%_ if ('id' === result.COLUMN_NAME) { _%>
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @ApiModelProperty(value = "主键ID")
     @Column(name = "id")
     private Integer id;
-
+        <%_ } else { _%>
+            <%_ if ('YES' === result.IS_NULLABLE) { _%>
     @NotNull
     @ApiParam(required = true)
-    @ApiModelProperty(value = "浮点数类型字段")
-    @Column(name = "double_field")
-    private Double doubleField;
-
-    @ApiModelProperty(value = "整数类型字段")
-    @Column(name = "integer_field")
-    private Integer integerField;
-
-    @NotNull
-    @ApiParam(required = true)
-    @ApiModelProperty(value = "字符串类型字段")
-    @Column(name = "string_field")
-    private String stringField;
-
-    @NotNull
-    @ApiModelProperty(value = "日期类型字段")
-    @Column(name = "date")
-    private Date date;
-
-    @ApiModelProperty(value = "布尔类型字段")
-    @Column(name = "bool")
-    private Boolean bool;
+            <%_ } _%>
+    @ApiModelProperty(value = "<%= result.COLUMN_COMMENT %>")
+    @Column(name = "<%= result.COLUMN_NAME %>")
+    private <%= result.DATA_TYPE %> <%= result.COLUMN_NAME%>
+        <%_ } _%>
+    <%_ } _%>
 }
