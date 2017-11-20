bb99cb03f32b3572d709ed18b90249cdfc048a50
hookszhang <hookszhang@gmail.com>
2017-11-20 13:45:24 +0800

    Update template _ModelResource.java

--- generators/entity/templates/_ModelResource.java
+++ generators/entity/templates/_ModelResource.java
@@ -1,9 +1,9 @@
-package com.zdan91.smart.bill.web.rest;
+package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.web.rest;
 
 import com.baomidou.mybatisplus.plugins.Page;
-import com.zdan91.smart.bill.model.SampleModel;
-import com.zdan91.smart.bill.service.SampleModelService;
-import com.zdan91.smart.bill.web.rest.vo.ResponseVO;
+import <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.model.<%= entityClass %>;
+import <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.service.<%= entityClass %>Service;
+import <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.web.rest.vo.ResponseVO;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
@@ -13,82 +13,82 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 /**
- * Rest controller. SampleModel
+ * Rest controller. <%= entityClass %>
  */
 @RestController
 @RequestMapping("/api")
-public class SampleModelResource {
+public class <%= entityClass %>Resource {
 
-    private Logger log = LoggerFactory.getLogger(EleBankResource.class);
+    private Logger log = LoggerFactory.getLogger(<%= entityClass %>Resource.class);
 
-    private final SampleModelService service;
+    private final <%= entityClass %>Service service;
 
-    public SampleModelResource(SampleModelService service) {
+    public <%= entityClass %>Resource(<%= entityClass %>Service service) {
         this.service = service;
     }
 
     /**
-     * Get /sample_model : Create a new sample_model
+     * Get /<%= tableName %> : Create a new <%= tableName %>
      *
-     * @param sampleModel the sample_model to create
-     * @return the ResponseEntity with status 200 (OK) and with body the new sampleModel
+     * @param <%= entityClassCamelCase %> the <%= tableName %> to create
+     * @return the ResponseEntity with status 200 (OK) and with body the new <%= entityClassCamelCase %>
      */
-    @PostMapping("/sample_model")
-    public ResponseVO createSampleModel(@Valid @RequestBody SampleModel sampleModel) {
-        log.debug("REST request to save SampleModel : {}", sampleModel);
-        service.insert(sampleModel);
+    @PostMapping("/<%= tableName %>")
+    public ResponseVO create<%= entityClass %>(@Valid @RequestBody <%= entityClass %> <%= entityClassCamelCase %>) {
+        log.debug("REST request to save <%= entityClass %> : {}", <%= entityClassCamelCase %>);
+        service.insert(<%= entityClassCamelCase %>);
         return ResponseVO.response().build();
     }
 
     /**
-     * PUT /sample_model : Updates an existing sample_model
+     * PUT /<%= tableName %> : Updates an existing <%= tableName %>
      *
-     * @param sampleModel the sample_model to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated sample_model
+     * @param <%= entityClassCamelCase %> the <%= tableName %> to update
+     * @return the ResponseEntity with status 200 (OK) and with body the updated <%= tableName %>
      */
-    @PutMapping("/sample_model")
-    public ResponseVO updateSampleModel(@Valid @RequestBody SampleModel sampleModel) {
-        log.debug("REST request to update SampleModel : {}", sampleModel);
-        service.updateById(sampleModel);
+    @PutMapping("/<%= tableName %>")
+    public ResponseVO update<%= entityClass %>(@Valid @RequestBody <%= entityClass %> <%= entityClassCamelCase %>) {
+        log.debug("REST request to update <%= entityClass %> : {}", <%= entityClassCamelCase %>);
+        service.updateById(<%= entityClassCamelCase %>);
         return ResponseVO.response().build();
     }
 
     /**
-     * GET /sample_model : get all sample_model.
+     * GET /<%= tableName %> : get all <%= tableName %>.
      *
      * @param pageable the pagination information
-     * @return the ResponseEntity with status 200 (OK) and with body all ele_bank
+     * @return the ResponseEntity with status 200 (OK) and with body all <%= tableName %>
      */
-    @GetMapping("/sample_model")
-    @ApiOperation(value = "get all sample_model.", response = Page.class)
-    public ResponseVO getAllSampleModel(@ApiParam Page pageable) {
-        final Page<SampleModel> page = service.selectPage(pageable);
+    @GetMapping("/<%= tableName %>")
+    @ApiOperation(value = "get all <%= tableName %>.", response = Page.class)
+    public ResponseVO getAll<%= entityClass %>(@ApiParam Page pageable) {
+        final Page<<%= entityClass %>> page = service.selectPage(pageable);
         return ResponseVO.response().setData(page).build();
     }
 
     /**
-     * GET /sample_model/:id : get the "id" sample_model
+     * GET /<%= tableName %>/:id : get the "id" <%= tableName %>
      *
-     * @param id the id of the sample_model to find
+     * @param id the id of the <%= tableName %> to find
      * @return
      */
-    @GetMapping("/sample_model/{id}")
-    @ApiOperation(value = "get the \"id\" sample_model", response = SampleModel.class)
-    public ResponseVO getSampleModel(@PathVariable Integer id) {
-        log.debug("REST request to get SampleModel : {}", id);
-        SampleModel entity = service.selectById(id);
+    @GetMapping("/<%= tableName %>/{id}")
+    @ApiOperation(value = "get the \"id\" <%= tableName %>", response = <%= entityClass %>.class)
+    public ResponseVO get<%= entityClass %>(@PathVariable Integer id) {
+        log.debug("REST request to get <%= entityClass %> : {}", id);
+        <%= entityClass %> entity = service.selectById(id);
         return ResponseVO.response().wrapOrNotFound(entity).build();
     }
 
     /**
-     * DELETE /sample_model/:id : delete the "id" sample_model.
+     * DELETE /<%= tableName %>/:id : delete the "id" <%= tableName %>.
      *
-     * @param id the id of the sample_model to delete
+     * @param id the id of the <%= tableName %> to delete
      * @return
      */
-    @DeleteMapping("/sample_model/{id}")
-    public ResponseVO deleteSampleModel(@PathVariable Integer id) {
-        log.debug("REST request to delete SampleModel : {}", id);
+    @DeleteMapping("/<%= tableName %>/{id}")
+    public ResponseVO delete<%= entityClass %>(@PathVariable Integer id) {
+        log.debug("REST request to delete <%= entityClass %> : {}", id);
         service.deleteById(id);
         return ResponseVO.response().build();
     }
