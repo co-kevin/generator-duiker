1f4c07b762b122f0052e242023affb6ba1b0b96a
hookszhang <hookszhang@gmail.com>
2017-11-21 14:10:17 +0800

    Update index.js, add trim comment

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -172,7 +172,13 @@ module.exports = class extends Generator {
         console.warn(`We don't catch this data type: ${column.DATA_TYPE}`)
       }
       column.fieldName = _string.camelCase(column.COLUMN_NAME)
+      column.COLUMN_COMMENT = _trim(column.COLUMN_COMMENT)
     }
+  }
+
+  // 删除字段注释中的换行符, 防止生成 Java 代码时编译错误
+  _trim(str) {
+    return str.replaceAll('\n', ' ')
   }
 
   _copyModel(columns, tableName, tableComment, nameCases, groupCases) {
