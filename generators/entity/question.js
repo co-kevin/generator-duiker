f72cbd0524838aef08a69896045acde086512483
zhanghua <zhanghua@91zdan.com>
2017-11-23 18:39:48 +0800

    Update questions.js, add method askTables

--- generators/entity/question.js
+++ generators/entity/question.js
@@ -1,38 +1,56 @@
+/**
+ * Prompt questions.
+ *
+ * @link https://github.com/SBoudrias/Inquirer.js#documentation
+ * @author hookszhang
+ */
 module.exports = class {
+
+  /**
+   * Require Yeoman Generator instance.
+   *
+   * @param {*yeoman-generator} generator
+   */
   constructor(generator) {
     this.generator = generator
   }
 
-  // 询问连接 MySQL 参数
+  /**
+   * Ask the user MySQL connection URL.
+   * URL format: mysql://user:pass@host/db?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700
+   */
   askConnection() {
+    const questions = [{
+      type: 'input',
+      name: 'mysqlURL',
+      message: 'Enter your MySQL URL',
+      default: this.mysqlURL,
+      store: true
+    }]
     return new Promise((resolve, reject) => {
-      const questions = [{
-        type: 'input',
-        name: 'mysqlURL',
-        message: 'Enter your MySQL URL',
-        default: this.mysqlURL,
-        store: true
-      }]
       this.generator.prompt(questions).then((answers) => {
         resolve(answers.mysqlURL)
       })
     })
   }
 
-  // 询问 database
-  askDB() {
-    const selectDBQuestion = [
-      {
-        type: 'checkbox',
-        name: 'tables',
-        message: 'Choices your DB',
-        choices: ['db1', 'db2']
-      }
-    ]
-  }
-
-  // 询问 tables
-  askTables() {
-
+  /**
+   * Ask the user to generate the entity from which tables.
+   * Multiple choices.
+   *
+   * @param {*Array} tables
+   */
+  askTables(tables) {
+    const questions = [{
+      type: 'checkbox',
+      name: 'tables',
+      message: 'From which tables to generate the entities',
+      choices: tables
+    }]
+    return new Promise((resolve, reject) => {
+      this.generator.prompt(questions).then((answers) => {
+        resolve(answers.tables)
+      })
+    })
   }
 }
