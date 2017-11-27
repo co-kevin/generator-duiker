38c75358a5819b54f471c4dced4c61c7b0a39f96
hookszhang <hookszhang@gmail.com>
2017-11-27 16:43:37 +0800

    Use project.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,6 @@
 const Generator = require('yeoman-generator')
 const _string = require('lodash/string')
-const _utils = require('../utils')
+const Project = require('./project')
 
 module.exports = class extends Generator {
   // The name `constructor` is important here
@@ -35,79 +35,6 @@ module.exports = class extends Generator {
 
   // callback when answered
   _run(answers) {
-    answers.nameCases = _utils.nameCase(answers.name)
-    answers.groupCases = _utils.groupCase(answers.group)
-
-    this._copyStaticFiles()
-    this._copyTplJava(answers)
-    this._copyTplYml(answers)
-    this._copyOther(answers)
-  }
-
-  // 输出所有 java 格式的模板文件
-  _copyTplJava(data) {
-    const baseTplPath = 'src/main/java/package'
-    const baseDestPath = `src/main/java/${data.groupCases.splitBySlash}/${data.nameCases.splitBySlash}`
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/_Application.java`)
-      , this.destinationPath(`${baseDestPath}/${data.nameCases.hump}Application.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/config/_Constants.java`)
-      , this.destinationPath(`${baseDestPath}/config/Constants.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/config/_LiquibaseConfiguration.java`)
-      , this.destinationPath(`${baseDestPath}/config/LiquibaseConfiguration.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/config/_MybatisConfiguration.java`)
-      , this.destinationPath(`${baseDestPath}/config/MybatisConfiguration.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/config/_SwaggerConfiguration.java`)
-      , this.destinationPath(`${baseDestPath}/config/SwaggerConfiguration.java`), data)
-      this.fs.copyTpl(this.templatePath(`${baseTplPath}/web/rest/vo/_ResponseVO.java`)
-      , this.destinationPath(`${baseDestPath}/web/rest/vo/ResponseVO.java`), data)
-    this._copyPackageInfo(data)
-  }
-
-  // 输出所有 package-info.java
-  _copyPackageInfo(data) {
-    const baseTplPath = 'src/main/java/package'
-    const baseDestPath = `src/main/java/${data.groupCases.splitBySlash}/${data.nameCases.splitBySlash}`
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/exception/_package-info.java`), this.destinationPath(`${baseDestPath}/exception/package-info.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/model/_package-info.java`), this.destinationPath(`${baseDestPath}/model/package-info.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/enums/_package-info.java`), this.destinationPath(`${baseDestPath}/enums/package-info.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/service/_package-info.java`), this.destinationPath(`${baseDestPath}/service/package-info.java`), data)
-    this.fs.copyTpl(this.templatePath(`${baseTplPath}/web/rest/_package-info.java`), this.destinationPath(`${baseDestPath}/web/rest/package-info.java`), data)
-  }
-
-  // 输出所有 yml 格式的模板文件
-  _copyTplYml(data) {
-    this.fs.copyTpl(this.templatePath('src/main/resources/_application.yml')
-      , this.destinationPath(`src/main/resources/application.yml`), data)
-    this.fs.copyTpl(this.templatePath('src/main/resources/_application-dev.yml')
-      , this.destinationPath(`src/main/resources/application-dev.yml`), data)
-    this.fs.copyTpl(this.templatePath('src/main/resources/_application-test.yml')
-      , this.destinationPath(`src/main/resources/application-test.yml`), data)
-    this.fs.copyTpl(this.templatePath('src/main/resources/_application-prod.yml')
-      , this.destinationPath(`src/main/resources/application-prod.yml`), data)
-  }
-
-  // 输出零碎文件
-  _copyOther(data) {
-    this.fs.copyTpl(this.templatePath('_build.gradle'), this.destinationPath(`build.gradle`), data)
-    this.fs.copyTpl(this.templatePath(`_README.md`), this.destinationPath(`README.md`), data)
-    this.fs.copyTpl(this.templatePath(`src/main/resources/_logback-spring.xml`), this.destinationPath(`src/main/resources/logback-spring.xml`), data)
-  }
-
-  _copyStaticFiles() {
-    let files = [
-      'gradle/wrapper/gradle-wrapper.jar',
-      'gradle/wrapper/gradle-wrapper.properties',
-      'gradle/mybatis_generate.gradle',
-      '.editorconfig',
-      '.gitignore',
-      'gradlew',
-      'gradlew.bat',
-      'CHANGELOG.md',
-      'src/main/resources/liquibase/master.xml'
-    ]
-
-    for (let file of files) {
-      this.fs.copy(this.templatePath(file), this.destinationPath(file))
-    }
+    new Project(this, answers).create()
   }
 }
--- generators/app/project.js
+++ generators/app/project.js
@@ -1,3 +1,5 @@
+const _utils = require('../utils')
+
 /**
  * Create a empty micro service project.
  *
@@ -16,22 +18,21 @@ module.exports = class {
    * Constructor a project by params.
    *
    * @param {*yeoman-generator} generator
-   * @param {*string} nameCases
-   * @param {*string} groupCases
+   * @param {*object} answers
    */
-  constructor(generator, nameCases, groupCases, port) {
+  constructor(generator, answers) {
     this.generator = generator
-    this.nameCases = nameCases
-    this.groupCases = groupCases
-    this.port = port
+    this.answers = answers
   }
 
   /**
    * Create a empty micro service project.
-   *
    */
   create() {
-
+    const data = this._data()
+    this._createStaticFiles(data)
+    this._createJavaCode(data)
+    this._createConfigYML(data)
   }
 
   /**
@@ -39,7 +40,31 @@ module.exports = class {
    * Transform from nameCases, groupCases, port
    */
   _data() {
+    this.answers.nameCases = _utils.nameCase(this.answers.name)
+    this.answers.groupCases = _utils.groupCase(this.answers.group)
+    return this.answers
+  }
 
+  /**
+   * Create java code files.
+   *
+   * @param {*object} data
+   */
+  _createJavaCode(data) {
+    const baseTplPath = 'src/main/java/package'
+    const baseDestPath = `src/main/java/${data.groupCases.splitBySlash}/${data.nameCases.splitBySlash}`
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/_Application.java`)
+      , this.generator.destinationPath(`${baseDestPath}/${data.nameCases.hump}Application.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/config/_Constants.java`)
+      , this.generator.destinationPath(`${baseDestPath}/config/Constants.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/config/_LiquibaseConfiguration.java`)
+      , this.generator.destinationPath(`${baseDestPath}/config/LiquibaseConfiguration.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/config/_MybatisConfiguration.java`)
+      , this.generator.destinationPath(`${baseDestPath}/config/MybatisConfiguration.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/config/_SwaggerConfiguration.java`)
+      , this.generator.destinationPath(`${baseDestPath}/config/SwaggerConfiguration.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/web/rest/vo/_ResponseVO.java`)
+      , this.generator.destinationPath(`${baseDestPath}/web/rest/vo/ResponseVO.java`), data)
   }
 
   /**
@@ -47,8 +72,51 @@ module.exports = class {
    * File List:
    * package-info.java, .gitignore, .editorconfig, README.md
    * gradlew, gradlew.bat, etc...
+   *
+   * @param {*object} data
    */
-  _createStaticFiles() {
+  _createStaticFiles(data) {
+    const files = [
+      'gradle/wrapper/gradle-wrapper.jar',
+      'gradle/wrapper/gradle-wrapper.properties',
+      '.editorconfig',
+      '.gitignore',
+      'gradlew',
+      'gradlew.bat',
+      'CHANGELOG.md',
+      'src/main/resources/liquibase/master.xml'
+    ]
 
+    for (const file of files) {
+      this.generator.fs.copy(this.generator.templatePath(file), this.generator.destinationPath(file))
+    }
+
+    const baseTplPath = 'src/main/java/package'
+    const baseDestPath = `src/main/java/${data.groupCases.splitBySlash}/${data.nameCases.splitBySlash}`
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/exception/_package-info.java`), this.generator.destinationPath(`${baseDestPath}/exception/package-info.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/model/_package-info.java`), this.generator.destinationPath(`${baseDestPath}/model/package-info.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/enums/_package-info.java`), this.generator.destinationPath(`${baseDestPath}/enums/package-info.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/service/_package-info.java`), this.generator.destinationPath(`${baseDestPath}/service/package-info.java`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`${baseTplPath}/web/rest/_package-info.java`), this.generator.destinationPath(`${baseDestPath}/web/rest/package-info.java`), data)
+
+    this.generator.fs.copyTpl(this.generator.templatePath('_build.gradle'), this.generator.destinationPath(`build.gradle`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`_README.md`), this.generator.destinationPath(`README.md`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath(`src/main/resources/_logback-spring.xml`), this.generator.destinationPath(`src/main/resources/logback-spring.xml`), data)
+  }
+
+  /**
+   * Create spring application config yml files.
+   *
+   * @param {*object} data
+   */
+  _createConfigYML(data) {
+    this.generator.fs.copyTpl(this.generator.templatePath('src/main/resources/_application.yml')
+      , this.generator.destinationPath(`src/main/resources/application.yml`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath('src/main/resources/_application-dev.yml')
+      , this.generator.destinationPath(`src/main/resources/application-dev.yml`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath('src/main/resources/_application-test.yml')
+      , this.generator.destinationPath(`src/main/resources/application-test.yml`), data)
+    this.generator.fs.copyTpl(this.generator.templatePath('src/main/resources/_application-prod.yml')
+      , this.generator.destinationPath(`src/main/resources/application-prod.yml`), data)
   }
 }
