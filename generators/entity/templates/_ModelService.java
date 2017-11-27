89c64b3776cde882c34392193f1fbb40284adfe4
zhanghua <zhanghua@91zdan.com>
2017-11-27 13:21:22 +0800

    Update _ModelService.java

--- generators/entity/templates/_ModelService.java
+++ generators/entity/templates/_ModelService.java
@@ -4,9 +4,9 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.mapper.<%= entityClass %>Mapper;
 import <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.model.<%= entityClass %>;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Transactional
+@Slf4j
 public class <%= entityClass %>Service extends ServiceImpl<<%= entityClass %>Mapper, <%= entityClass %>> {
 }
