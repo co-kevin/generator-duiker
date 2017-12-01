c28a4c1bc1d58dadcabc8e1b5fcb339924bfed5e
zhanghua <zhanghua@91zdan.com>
2017-12-01 14:00:10 +0800

    Update _MybatisConfiguration

--- generators/app/templates/src/main/java/package/config/_MybatisConfiguration.java
+++ generators/app/templates/src/main/java/package/config/_MybatisConfiguration.java
@@ -2,6 +2,7 @@ package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.config;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
