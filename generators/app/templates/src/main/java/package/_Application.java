c3850cd39bf2ee9c58840e5d80d1ae67ab939f9f
zhanghua <zhanghua@91zdan.com>
2017-12-06 13:30:47 +0800

    Update java template

--- generators/app/templates/src/main/java/package/_Application.java
+++ generators/app/templates/src/main/java/package/_Application.java
@@ -1,11 +1,15 @@
 package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableFeignClients
+@MapperScan("<%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.mapper*")
 public class <%= nameCases.hump %>Application {
 
     public static void main(String[] args) {
