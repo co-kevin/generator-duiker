4adf9caa51400e4269290c136f5a7c977e5a1474
hookszhang <hookszhang@gmail.com>
2017-12-14 17:08:07 +0800

    Add file _MybatisConfiguration.java

--- generators/app/templates/src/main/java/package/_Application.java
+++ generators/app/templates/src/main/java/package/_Application.java
@@ -1,6 +1,5 @@
 package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,7 +8,6 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
-@MapperScan("<%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.mapper*")
 public class <%= nameCases.hump %>Application {
 
     public static void main(String[] args) {
---
+++ generators/app/templates/src/main/java/package/config/_MybatisConfiguration.java
@@ -0,0 +1,10 @@
+package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.config;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("<%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.mapper*")
+public class MybatisPlusConfig {
+}
