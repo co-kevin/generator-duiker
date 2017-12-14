59adf08babcd1aaeddb2019522623431f3faa0f4
zhanghua <zhanghua@91zdan.com>
2017-12-14 17:41:27 +0800

    Bug Fix: mybatis plus config

--- generators/app/templates/src/main/java/package/config/_MybatisConfiguration.java
+++ generators/app/templates/src/main/java/package/config/_MybatisConfiguration.java
@@ -1,10 +1,9 @@
 package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.config;
 
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan("<%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.mapper*")
-public class MybatisPlusConfig {
+public class MybatisConfiguration {
 }
