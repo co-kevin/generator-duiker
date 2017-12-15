4101f3a351d512a79754faac6bba683e0c776ca2
hookszhang <hookszhang@gmail.com>
2017-12-15 14:00:24 +0800

    Bug Fix: swagger configuration, mapper scan

--- generators/app/templates/src/main/java/package/config/_MybatisConfiguration.java
+++
@@ -1,9 +0,0 @@
-package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("<%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.mapper*")
-public class MybatisConfiguration {
-}
