318ea0eaa356015561be37a7ebc0452fda48331e
hookszhang <hookszhang@gmail.com>
2018-04-10 14:02:55 +0800

    Fix kotlin Constants

--- generators/service/templates/src/main/kotlin/package/config/_Constants.kt
+++ generators/service/templates/src/main/kotlin/package/config/_Constants.kt
@@ -4,12 +4,11 @@ package <%= groupCases.splitByDot %>.<%= nameCases.splitByDot %>.config;
  * @author hookszhang on 30/10/2017.
  */
 object Constants {
-    val SPRING_PROFILE_DEVELOPMENT = "dev"
-    val SPRING_PROFILE_TEST = "test"
-    val SPRING_PROFILE_PRODUCTION = "prod"
+    const val SPRING_PROFILE_DEVELOPMENT = "dev"
+    const val SPRING_PROFILE_TEST = "test"
+    const val SPRING_PROFILE_PRODUCTION = "prod"
     // Spring profile used to disable swagger
-    val SPRING_PROFILE_SWAGGER = "swagger"
+    const val SPRING_PROFILE_SWAGGER = "swagger"
     // Spring profile used to disable running liquibase
-    val SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase"
+    const val SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase"
 }
-
